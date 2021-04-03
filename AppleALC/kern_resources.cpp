b0324cd4c5f085d7f5ebadb478c091bc2a5c6cd2
vit9696 <vit9696@users.noreply.github.com>
2021-04-03 16:47:53 +0300

    Add dummy resources

---
+++ AppleALC/kern_resources.cpp
@@ -0,0 +1,19 @@
+//
+//  kern_resources.cpp
+//  AppleALC
+//
+//  Created by user on 03.04.2021.
+//  Copyright © 2021 vit9696. All rights reserved.
+//
+
+#include "kern_resources.hpp"
+
+unsigned char platformData[] = {
+	1, 2, 3,
+};
+unsigned int platformDataSize = sizeof(platformData);
+
+unsigned char layoutData[] = {
+	1, 2, 3,
+};
+unsigned int layoutDataSize = sizeof(layoutData);
